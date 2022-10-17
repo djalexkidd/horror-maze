8441a439c53f89e7d166b280adb8e88a27616d02
djalexkidd <alexpyr@outlook.fr>
2022-10-17 21:03:11 +0200

    Le meilleur jeu du monde

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,4 +1,11 @@
 extends Control
 
-func _on_Button_pressed():
+func _on_StartButton_pressed():
 	get_tree().change_scene("res://scenes/Game.tscn")
+
+#Active/Désactive le plein écran
+func _on_FullScreenButton_pressed():
+	if OS.window_fullscreen:
+		OS.window_fullscreen = false
+	else:
+		OS.window_fullscreen = true
