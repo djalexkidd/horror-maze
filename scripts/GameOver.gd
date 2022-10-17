e0149bc799df3015144e2d8cf91d9baf2437b18f
djalexkidd <alexpyr@outlook.fr>
2022-10-17 20:51:43 +0200

    Ajout de boutons sur l'écran de Game Over

--- scripts/GameOver.gd
+++ scripts/GameOver.gd
@@ -5,3 +5,9 @@ func _on_Timer_timeout():
 
 func _on_Boo_finished():
 	$Out.play()
+
+func _on_RestartButton_pressed():
+	get_tree().change_scene("res://scenes/Menu.tscn")
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
