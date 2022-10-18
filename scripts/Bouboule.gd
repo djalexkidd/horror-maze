49be19b0cde9bbe5b7663158b540762c68e1637d
djalexkidd <alexpyr@outlook.fr>
2022-10-18 09:27:12 +0200

    Niveau 4

--- scripts/Bouboule.gd
+++ scripts/Bouboule.gd
@@ -2,12 +2,15 @@ extends KinematicBody2D
 
 var speed = 100 #Vitesse
 var velocity = Vector2()
-export var direction = 1
+export var directiony = 1
+export var directionx = 0
 
 func _physics_process(delta):
-	velocity.y = speed * direction
+	velocity.y = speed * directiony
+	velocity.x = speed * directionx
 	
 	velocity = move_and_slide(velocity,Vector2.UP)
 	
-	if is_on_floor() or is_on_ceiling():
-		direction = direction * -1
+	if is_on_floor() or is_on_ceiling() or is_on_wall():
+		directiony = directiony * -1
+		directionx = directionx * -1
--- scripts/levels/Level3.gd
+++ scripts/levels/Level3.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _on_Goal_area_entered(area):
-	get_tree().change_scene("res://scenes/Menu.tscn")
+	get_tree().change_scene("res://scenes/levels/Level4.tscn")
