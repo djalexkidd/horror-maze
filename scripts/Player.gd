81253e4e98fc8f215d1673fa9b60d725f5d530b6
djalexkidd <alexpyr@outlook.fr>
2022-11-02 17:41:31 +0100

    Tween player movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
-func _physics_process(delta):
-	position = get_viewport().get_mouse_position() # Suit le mouvement de la souris
+#Suit le mouvement de la souris sur l'axe X
+func _on_MoveTimer_timeout():
+	var tween = get_node("Tween")
+	tween.interpolate_property(self, "position",Vector2(position.x,position.y), Vector2(get_viewport().get_mouse_position().x,get_viewport().get_mouse_position().y),0.05 ,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
 
 func _on_Player_body_entered(body):
 	Global.stop_time()
